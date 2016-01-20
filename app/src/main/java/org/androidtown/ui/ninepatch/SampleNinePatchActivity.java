9e097629bcfe9a6ed2d23e8dfbe07b5767dacc2a
raindrops29 <raindrops29@empas.com>
2016-01-21 08:53:12 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/ui/ninepatch/SampleNinePatchActivity.java
@@ -0,0 +1,14 @@
+package org.androidtown.ui.ninepatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleNinePatchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
